9f5875926b72d267eea11970ea2fc31c5377e319
Aff-Nst <ahmadfuadifirdausnst@gmail.com>
2024-04-24 21:36:35 +0700

    upload tugas pertemuan 1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>PERKENALKAN</h1>";
+        echo "Nama saya Ahmad Fuadi Firdaus Nasution, Lahir dan menetap sebagai warga Bekasi Timur";
+    }
+}
